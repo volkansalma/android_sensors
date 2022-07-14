bfceacc77b602ac8564d1831dfa4a9f48f8f9c46
Volkan Salma <volkansalma@yahoo.com>
2022-07-14 15:21:18 +0200

    pygame is added for visualizing the values

--- androidwebsocket.py
+++ androidwebsocket.py
@@ -1,10 +1,12 @@
 from threading import Thread
+import threading
 from time import sleep
 import websocket
 import json
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import signal
+import pygame as pg
 
 #max acc read rate 2ms, 500Hz
 class MobileSensorReceiver:
@@ -19,7 +21,7 @@ class MobileSensorReceiver:
 
         self.Acc_sensor_bias = np.zeros((1, 3), dtype = np.float64)
         self.Acc_sensor_stddev = np.zeros((1, 3), dtype = np.float64)
-        self.Acc_cal_buffer = np.zeros((500, 4), dtype = np.float64)
+        self.Acc_cal_buffer = np.zeros((5000, 4), dtype = np.float64)
         self.Acc_median7_filter_buffer = np.zeros((7, 3), dtype = np.float64)
 
         self.ws = websocket.WebSocketApp("ws://192.168.41.17:8081/sensor/connect?type=android.sensor.accelerometer",
@@ -40,6 +42,11 @@ class MobileSensorReceiver:
         self.debug_curr_data = np.zeros((500, 3), dtype=np.float64)
         self.debug_median_filtered_data = np.zeros((500, 3), dtype=np.float64)
 
+    def start(self):
+        wst = threading.Thread(target=self.ws.run_forever)
+        wst.daemon = True
+        wst.start()
+
     def acc_median7_filter_init(self):
         #fills the last 7 data from the calibration to initialize the filter
         #data in the calibration buffer is median filtered raw data, so sensor bias needs to be substracted
@@ -56,22 +63,22 @@ class MobileSensorReceiver:
         return median
 
     
-    def calculate_velocity(self, Acc_curr, data_timestamp):
+    def calculate_velocity(self, data_timestamp):
         time_diff_s = (data_timestamp - self.prev_acc_update_timestamp) * self.nanosecs2secs_coef
         
         #use the average accelaration for more accurate integration
-        self.Vel_curr = self.Vel_prev + (Acc_curr + self.Acc_prev) * 0.5 * time_diff_s
+        self.Vel_curr = self.Vel_prev + (self.Acc_curr + self.Acc_prev) * 0.5 * time_diff_s
         
         #fill the values for the next cycle
         self.Vel_prev = self.Vel_curr  
         self.prev_vel_update_timestamp = data_timestamp
         
 
-    def calculate_position(self, Vel_curr, data_timestamp):
-        time_diff_s = (data_timestamp - self.prev_vel_update_timestamp) * self.nanosecs2secs_coef
+    def calculate_position(self, data_timestamp):
+        time_diff_s = (data_timestamp - self.prev_pos_update_timestamp) * self.nanosecs2secs_coef
         
         #use the average velocity for more accurate integration
-        self.Pos_curr = self.Pos_prev + (Vel_curr + self.Vel_prev) * 0.5 * time_diff_s
+        self.Pos_curr = self.Pos_prev + (self.Vel_curr + self.Vel_prev) * 0.5 * time_diff_s
         
         #fill the values for the next cycle
         self.Pos_prev = self.Pos_curr 
@@ -91,7 +98,7 @@ class MobileSensorReceiver:
         self.Acc_cal_buffer[self.acc_cal_data_cnt] = [values[0], values[1], values[2], data_timestamp]
         self.acc_cal_data_cnt += 1
 
-        if(self.acc_cal_data_cnt >= 500): #1000 samples 2sn
+        if(self.acc_cal_data_cnt >= 5000): #1000 samples 2sn
             
             #sliding median filter (7th order) is applied on raw data to get rid of the spikes. 
             medX = signal.medfilt(self.Acc_cal_buffer[:, 0], kernel_size=7)
@@ -103,7 +110,7 @@ class MobileSensorReceiver:
             self.Acc_sensor_stddev[0] = [np.std(medX, dtype = np.float64), np.std(medY, dtype = np.float64), np.std(medZ, dtype = np.float64)]
             
             #np.savetxt('Acc_cal_buffer.csv', self.Acc_cal_buffer, delimiter=',')
-            #print("Calibration completed: ", self.Acc_sensor_bias, self.Acc_sensor_stddev)
+            print("Calibration completed: ", self.Acc_sensor_bias, self.Acc_sensor_stddev)
             #plt.plot(self.Acc_cal_buffer[:,3], self.Acc_cal_buffer[:,0])
             #plt.plot(self.Acc_cal_buffer[:,3])
             #plt.show()
@@ -122,30 +129,27 @@ class MobileSensorReceiver:
         #self.debug_raw_data[499] = Acc_read_raw
 
         #rectify the raw acc reading with substracting the bias
-        Acc_curr = Acc_read_raw - self.Acc_sensor_bias
+        Acc_bias_corrected = Acc_read_raw - self.Acc_sensor_bias
         #self.debug_curr_data = np.roll(self.debug_curr_data, -1, axis=0)
-        #self.debug_curr_data[499] = Acc_curr
+        #self.debug_curr_data[499] = Acc_bias_corrected
 
-        Acc_median_filtered = self.acc_median7_filter(Acc_curr)
+        Acc_median_filtered = self.acc_median7_filter(Acc_bias_corrected)
         #self.debug_median_filtered_data = np.roll(self.debug_median_filtered_data, -1, axis=0)
         #self.debug_median_filtered_data[499] = Acc_median_filtered
 
-        #change = abs(Acc_curr[0, 0] - self.Acc_prev[0, 0])
+        self.Acc_curr[0] = Acc_median_filtered
+
+        #calculate the position and velocity by numerical integration
+        self.calculate_velocity(data_timestamp)
+        self.calculate_position(data_timestamp)
         
-        #ignore the small oscilations on acc to filter out noise
-        #if (change > (2 * self.Acc_sensor_stddev[0, 0])):
-            #calculate the position and velocity by numerical integration
-        
-        self.calculate_velocity(Acc_median_filtered, data_timestamp)
-        #self.Pos_curr = self.calculate_position(self.Vel_curr, data_timestamp)
-        
-        self.Acc_prev = Acc_median_filtered
+        self.Acc_prev = self.Acc_curr
         self.prev_acc_update_timestamp = data_timestamp
 
         #print the calculated values in 1 hz
         self.print_data_cnt += 1
-        if(self.print_data_cnt >= 250):
-            print("Vx(cm/s): Px(cm)", int(self.Vel_curr[0, 0] * 100), int(self.Pos_curr[0, 0] * 100))
+        if(self.print_data_cnt >= 500):
+            #print("Calculating...")
             self.print_data_cnt = 0
   
     def __on_acc_message(self, ws, message):
@@ -157,6 +161,58 @@ class MobileSensorReceiver:
             else:
                 self.process_acc_data(values, data_timestamp)
 
-if __name__ == "__main__":
+def draw_acc_calculations(screen, mobile_sensor_rec):
+    white=(255, 255, 255)
+    yellow=(255, 255, 0)
+    green=(0, 255, 255)
+    orange=(255, 100, 0)
+
+    pg.draw.line(screen, yellow, (250, 30), (250 + -1 * int(mobile_sensor_rec.Acc_curr[0, 0] * 100) , 30), 20) #Ax
+    pg.draw.line(screen, green, (250, 60), (250 + int(mobile_sensor_rec.Acc_curr[0, 1] * 100), 60), 20) #Ay
+    pg.draw.line(screen, orange, (250, 90), (250 + int(mobile_sensor_rec.Acc_curr[0, 2] * 100), 90), 20) #Az
+
+    #draw velocities
+    pg.draw.line(screen, white, (250, 150), (250 + int(mobile_sensor_rec.Vel_curr[0, 0] * 100) , 150), 20) #Vx
+    pg.draw.line(screen, orange, (250, 180), (250 + int(mobile_sensor_rec.Vel_curr[0, 1] * 100), 180), 20) #Vy
+    pg.draw.line(screen, yellow, (250, 210), (250 + int(mobile_sensor_rec.Vel_curr[0, 2] * 100), 210), 20) #Vz
+
+    #draw positions
+    pg.draw.line(screen, white, (250, 270), (250 + -1 * int(mobile_sensor_rec.Pos_curr[0, 0] * 100) , 270), 20) #Vx
+    pg.draw.line(screen, green, (250, 300), (250 + int(mobile_sensor_rec.Pos_curr[0, 1] * 100), 300), 20) #Vy
+    pg.draw.line(screen, orange, (250, 330), (250 + int(mobile_sensor_rec.Pos_curr[0, 2] * 100), 330), 20) #Vz
+
+def process_user_input(events):
+    for event in events:
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                return False
+                    
+        elif event.type == pg.QUIT:
+            return False
+    
+    return True
+
+if __name__ == "__main__":  
+    pg.init()
+    screen = pg.display.set_mode([500, 500])
+    pg.display.set_caption("Indoor positioning App")
+
+    clock = pg.time.Clock()
+    
     mobile_sensor_rec = MobileSensorReceiver()
-    mobile_sensor_rec.ws.run_forever()
+    mobile_sensor_rec.start()
+
+    while True:
+        clock.tick(60)
+        
+        screen.fill((0,0,0))
+        draw_acc_calculations(screen, mobile_sensor_rec)
+        
+        # Flip the display
+        pg.display.flip()
+
+        #check quit request
+        if (process_user_input(pg.event.get()) == False):
+            break
+
+    pg.quit()
